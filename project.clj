36e8b4e61e96824299fdc28d4aea1e215b71cc04
Bobby Calderwood <bobby_calderwood@mac.com>
2010-10-09 23:13:09 -0400

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject org.clojars.bobby/jzmq-clojars "2.0.9-SNAPSHOT"
+  :description "ZeroMQ Java bindings all packaged up and ready for use with leiningen + native-deps + clojars.org.
+                Only includes Mac OS X x86 native dependencies at this time.  Please fork ")
